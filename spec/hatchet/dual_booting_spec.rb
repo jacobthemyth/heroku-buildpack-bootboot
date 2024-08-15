251b0f876713e6b712a3087de822245db1866fbe
Jacob Smith <jacob@jacobsmith.io>
2024-08-15 10:03:35 -0400

    WIP

--- spec/hatchet/dual_booting_spec.rb
+++ spec/hatchet/dual_booting_spec.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
 describe "Dual booting" do
-  around(:each) do |spec|
-    Bundler.with_unbundled_env(&spec)
+  # around(:each) do |spec|
+  #   Bundler.with_unbundled_env(&spec)
+  # end
+
+  describe "bootboot_rails_upgrade" do
+    it "fails without this buildpack" do
+      buildpacks = ["heroku/ruby"]
+
+      Hatchet::Runner.new("spec/fixtures/repos/bootboot_rails_upgrade", buildpacks: buildpacks).deploy do |app|
+        expect(app.output).to match("qqqqqqqqq")
+      end
+    end
   end
 
-  it "fails without this buildpack" do
-    buildpacks = ["heroku/ruby"]
-
-    before_deploy = proc do
-      FileUtils.cp("Gemfile.lock", "Gemfile_next.lock")
-    end
-
-    Hatchet::Runner.new("default_ruby", buildpacks: buildpacks, before_deploy: before_deploy).deploy do |app|
-    end
-  end
-
-  describe "different Ruby versions" do
-    xit "handles apps with different Ruby versions in Gemfile.lock and Gemfile_next.lock" do
+  xdescribe "different Ruby versions" do
+    it "handles apps with different Ruby versions in Gemfile.lock and Gemfile_next.lock" do
       buildpacks = ["heroku/ruby", :default]
 
       before_deploy = proc do
