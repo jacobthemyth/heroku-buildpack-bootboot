a3caca83d428d193ce99217904077ce58cda98a4
Jacob Smith <jacob@jacobsmith.io>
2024-08-14 20:51:15 -0400

    WIP stop vendoring Ruby buildpack

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,2 @@
-$LOAD_PATH.unshift File.expand_path("../vendor/gems/language_pack/lib", __dir__)
-require "language_pack"
-
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "bootboot_buildpack"
